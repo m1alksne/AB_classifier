39185f2c81387a606d52f14b949dbfcbd171134e
sergeygurvich <segurvich@ucsd.edu>
2024-03-27 10:06:27 -0700

    comments

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 from pathlib import Path
 
 repo_path = Path(__file__).parent
+
+# Path to Data:
+# if inside repository
 xwavs_path = repo_path / 'labeled_data' / 'xwavs'
+
+# if absolute path is needed:
+# xwavs_path = Path('/srv/starter_content/_Shared-Storage_/AB_classifier/wav_files')
