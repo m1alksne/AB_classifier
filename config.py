fb6dbbb2397ebb0dc465910f61b2241594f3cd68
sergeygurvich <segurvich@ucsd.edu>
2024-03-26 20:56:54 -0700

    few fixes

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 repo_path = Path(__file__).parent
+xwavs_path = repo_path / 'labeled_data' / 'xwavs'
--- train/train.py
+++ train/train.py
@@ -1,4 +1,3 @@
-
 """
 Created on Mon Jan 22 11:35:46 2024
 
@@ -24,19 +23,18 @@ Spectrogram parameters:
     - 1600 Hamming window samples. A Hamming window is used to smooth the signal and reduce spectral leakage/artifacts for the FFT. 
     - minimum frequency: 10 Hz
     - maximum frequency: 150 Hz
-    
-Spectrogram augmentations: 
+
+Spectrogram augmentations:
     - frequency_mask: adds random horizontal bars over image
     - time_mask: adds random vertical bars over the image
-    - add_noise: adds random Gaussian noise to image 
-    
+    - add_noise: adds random Gaussian noise to image
+
 Notes for user:
 batch_size – number of training files to load/process before re-calculating the loss function and backpropagation
 num_workers – parallelization (ie, cores or cpus)
 log_interval – interval in epochs to evaluate model with validation dataset and print metrics to the log
 
 """
-
 import opensoundscape
 import glob
 import os
@@ -48,37 +46,43 @@ import torch
 import wandb
 import random
 
-from config import repo_path
+from config import repo_path, xwavs_path
 
 # read in train and validation dataframes
-train_clips = pd.read_csv(repo_path/'labeled_data'/'train_val_test_clips'/'train_clips.csv', index_col=[0,1,2])
-val_clips = pd.read_csv(repo_path/'labeled_data'/'train_val_test_clips'/'val_clips.csv', index_col=[0,1,2])
-print(train_clips.sum()) 
+train_clips = pd.read_csv(repo_path / 'labeled_data' / 'train_val_test_clips' / 'train_clips.csv')
+train_clips['file'] = str(xwavs_path) + '/' + train_clips['file']
+train_clips.set_index(['file', 'start_time', 'end_time'], inplace=True)
+
+val_clips = pd.read_csv(repo_path / 'labeled_data' / 'train_val_test_clips' / 'val_clips.csv')
+val_clips['file'] = str(xwavs_path) + '/' + val_clips['file']
+val_clips.set_index(['file', 'start_time', 'end_time'], inplace=True)
+
+print(train_clips.sum())
 print(val_clips.sum())
 
-calls_of_interest = ["A NE Pacific", "B NE Pacific"] #define the calls for CNN
-model = opensoundscape.CNN('resnet18',classes=calls_of_interest,sample_duration=30.0, single_target=False) # create a CNN object designed to recognize 30-second samples
-opensoundscape.ml.cnn.use_resample_loss(model, train_df=train_clips) # loss function for mult-target classification
+calls_of_interest = ["A NE Pacific", "B NE Pacific"]  # define the calls for CNN
+model = opensoundscape.CNN('resnet18', classes=calls_of_interest, sample_duration=30.0,
+                           single_target=False)  # create a CNN object designed to recognize 30-second samples
+opensoundscape.ml.cnn.use_resample_loss(model, train_df=train_clips)  # loss function for mult-target classification
 
 # moodify model preprocessing for making spectrograms the way I want them
 model.preprocessor.pipeline.to_spec.params.window_type = 'hamming'
-model.preprocessor.pipeline.to_spec.params.window_samples = 1600 
-model.preprocessor.pipeline.to_spec.params.overlap_samples = 1400 
-model.preprocessor.pipeline.to_spec.params.fft_size = 3200 
-model.preprocessor.pipeline.to_spec.params.decibel_limits = (-120,150)
+model.preprocessor.pipeline.to_spec.params.window_samples = 1600
+model.preprocessor.pipeline.to_spec.params.overlap_samples = 1400
+model.preprocessor.pipeline.to_spec.params.fft_size = 3200
+model.preprocessor.pipeline.to_spec.params.decibel_limits = (-120, 150)
 model.preprocessor.pipeline.to_spec.params.scaling = 'density'
 model.preprocessor.pipeline.bandpass.params.min_f = 10
 model.preprocessor.pipeline.bandpass.params.max_f = 150
 model.preprocessor.pipeline.frequency_mask.bypass = True
-#model.preprocessor.pipeline.time_mask.bypass = True
-#model.preprocessor.pipeline.frequency_mask.set(max_width = 0.003, max_masks=1)
-model.preprocessor.pipeline.time_mask.set(max_width = 0.1, max_masks=5)
+# model.preprocessor.pipeline.time_mask.bypass = True
+# model.preprocessor.pipeline.frequency_mask.set(max_width = 0.003, max_masks=1)
+model.preprocessor.pipeline.time_mask.set(max_width=0.1, max_masks=5)
 model.preprocessor.pipeline.add_noise.set(std=0.1)
-model.preprocessor.pipeline.random_affine.bypass=True
-model.optimizer_params['lr']=0.001
-model.lr_cooling_factor = 0.3 
-model.wandb_logging['n_preview_samples']=100 # number of samples to look at in wandB
-
+model.preprocessor.pipeline.random_affine.bypass = True
+model.optimizer_params['lr'] = 0.001
+model.lr_cooling_factor = 0.3
+model.wandb_logging['n_preview_samples'] = 100  # number of samples to look at in wandB
 
 # wandb_session = wandb.init( #initialize wandb logging
 #         entity='BigBlueWhale', #replace with your entity/group name
@@ -86,14 +90,14 @@ model.wandb_logging['n_preview_samples']=100 # number of samples to look at in w
 #         name='Trial 9: 30 second windows')
 
 model.train(
-    train_clips, 
-    val_clips, 
-    epochs = 12, 
-    batch_size= 128, 
-    log_interval=1, #log progress every 1 batches
-    num_workers = 12, 
-    #wandb_session=wandb_session,
-    save_interval = 1, #save checkpoint every 1 epoch
-    save_path = repo_path/'train'/'model_states' #location to save checkpoints (epochs)
+    train_clips,
+    val_clips,
+    epochs=12,
+    batch_size=128,
+    log_interval=1,  # log progress every 1 batches
+    num_workers=12,
+    # wandb_session=wandb_session,
+    save_interval=1,  # save checkpoint every 1 epoch
+    save_path=repo_path / 'train' / 'model_states'  # location to save checkpoints (epochs)
 )
 
